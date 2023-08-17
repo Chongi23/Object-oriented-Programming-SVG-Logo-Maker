050c69087b949c7a8313091cc7bc831b492dd845
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-08-16 19:34:58 -0500

    inquirer input works correctly set up initial paths to generate logos

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
+//Require inquirer to provide way to capture use input on Node.js CLI
 const inquirer = require('inquirer');
+const path = require('path');
+//Require ('fs) to RAED and WRITE to the file system
 const fs = require('fs');
-const fileName = "./Examples/logo.svg";
-const questions = require(/Lib/questions.js);
-const shapes = require('./lib.shapes.js')
+const { genShape } = require('./Lib/genShape');
+const { genSVG } = require('./Lib/genSVG');
 
-//Function to create SVG file using the inquirer promt response and file system(fs)
-function createLogo(response) {
-    const svg = shapes(response);
-    fs.writeFile(fileName, svg, () => 
-    console.log("SVG File Created Successfully!"));
-}
+ //Create an array of questions for user input in making SVG logo
 
-//Initialization, createLogo using question prompts(inquirer), will also catch errors
+ inquirer
+ .prompt([
+    {
+        type: 'input',
+        name:'logoName',
+        message: "Enter 3 letter for your logo, no more, no less",
+    },
+
+    {
+        type :'input',
+        name: 'logoTextColor',
+        message:"What is the color you want text to be?",
+    },
+
+    {
+        type: 'input',
+        name: 'logoColor',
+        message:"What color would you like your logo? (ex: #012456  OR color name)",
+    },
+
+    {
+        type: 'checkbox',
+        name: 'logoShape',
+        message: 'Choose a provided shape',
+        choices:['square', 'circle', 'triangle'],
+
+    },
+
+])
+//AFTER the user input , THEN we use that data ,
+//We have a svgPath var with value of "./Examples/logo.svg"
+//finalLogo will have the value of genShape imported function with the parameter of data(from input)
+.then((data) => {
+  const svgPath = "./Examples/logo.svg";
+  const finalLogo = genShape(data);
+
+  // Generates code to display logo
+  fs.writeFile(svgPath, genSVG(finalLogo), (err) =>
+    err
+      ? console.error(err)
+      : console.log("Successfully generated logo into logo.svg")
+  );
+})
+.catch((err) => console.error(err));
 
