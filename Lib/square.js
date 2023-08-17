fdd64a3db5813e58a64fb9c708d484b13c4f87a4
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-08-16 22:42:27 -0500

    Square render set up

---
+++ Lib/square.js
@@ -0,0 +1,14 @@
+const Shape = require("./shape.js");
+//The extends keyword can be used to subclass custom classes
+class Square extends Shape {
+  constructor(data) {
+    //The super() keyword in JavaScript is used to call the constructor of a parent class from a child class
+    super(data);
+  }
+  render() {
+    //The `return` statement is used to return an "element". The element is a basic HTML element that is used to draw a rectangle. The `width`, `height`, and `rx` attributes of the `` element are used to specify the width, height, and corner radius of the rectangle. The `fill` attribute is used to specify the color of the rectangle.
+    return `<rect width="100" height="100" rx="15" fill="${this.logoColor}" />`;
+  }
+}
+
+module.exports = Square;
